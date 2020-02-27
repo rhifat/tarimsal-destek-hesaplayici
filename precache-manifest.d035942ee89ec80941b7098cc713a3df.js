82accf6f42440cc7b5d0e37c7ffad455e7eb55e7
= <=>
2020-02-27 19:15:41 +0300

    Updates

---
+++ precache-manifest.d035942ee89ec80941b7098cc713a3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a567e4b516c0bd1a6a3",
+    "url": "/tarimsal-destek-hesaplayici/static/css/main.3a5c603b.chunk.css"
+  },
+  {
+    "revision": "1a567e4b516c0bd1a6a3",
+    "url": "/tarimsal-destek-hesaplayici/static/js/main.6aa6e9b4.chunk.js"
+  },
+  {
+    "revision": "c87a8b5fe5aa21bf9894",
+    "url": "/tarimsal-destek-hesaplayici/static/js/runtime~main.c196ffd4.js"
+  },
+  {
+    "revision": "718ac7dec31fc1e2b731",
+    "url": "/tarimsal-destek-hesaplayici/static/css/2.4b21050f.chunk.css"
+  },
+  {
+    "revision": "718ac7dec31fc1e2b731",
+    "url": "/tarimsal-destek-hesaplayici/static/js/2.74400b3c.chunk.js"
+  },
+  {
+    "revision": "d31aff467f53d02b8a3dc79139b91985",
+    "url": "/tarimsal-destek-hesaplayici/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Calculator from "./components/Calculator";
-
-ReactDOM.render(<Calculator />, document.getElementById("root"));
